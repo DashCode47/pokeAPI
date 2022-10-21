be2361d7021d52bf0a419ffcdee6f027b91c2127
David Lozada <davsh47@hotmail.com>
2022-10-21 15:29:31 +0300

    final

--- routes/pokeroute.js
+++ routes/pokeroute.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const pokemons = require("../models/pokemons");
 const router = express.Router();
 const Pokemon = require("../models/pokemons");
 //POST CREATE
@@ -21,4 +22,25 @@ router.post("/", (req, res) => {
     });
 });
 
+//GET METHOD
+router.get("/", async (req, res) => {
+  const pokemon = await Pokemon.find();
+  if (!pokemon) res.status(500).send("NOT getted");
+  res.send(pokemon);
+});
+//get by id
+router.get("/:pokemonId", async (req, res) => {
+  const pokemon = await Pokemon.findOne({
+    name: new RegExp(req.params.pokemonId, "i"),
+  });
+  if (!pokemon) res.status(500).send(error.message);
+  res.send(pokemon);
+});
+//DELETE ENTRY
+/* router.delete("/:pokeId", async (req, res) => {
+  const pokemon = await Pokemon.findByIdAndRemove(req.params.pokeId);
+  if (!pokemon) res.status(404).send("id not found");
+  res.send(book);
+});
+ */
 module.exports = router;
